bdcf11cc9302976be8b2dcde2fd4930518c03fcf
dan <dmsedra@gmail.com>
2018-02-19 12:15:23 -0800

    add small data

--- test/video_frame.py
+++ test/video_frame.py
@@ -1,20 +1,32 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+def run():
+	cap = cv2.VideoCapture(0)
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Display the resulting frame
-    cv2.imshow('frame',gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+	while(True):
+	    # Capture frame-by-frame
+	    ret, frame = cap.read()
 
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
+	    # Our operations on the frame come here
+	    #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY
+	    getRectangle(frame)
+
+	    # Display the resulting frame
+	    cv2.imshow('frame',frame)
+	    if cv2.waitKey(1) & 0xFF == ord('q'):
+	        break
+
+	# When everything done, release the capture
+	cap.release()
+	cv2.destroyAllWindows()
+
+def getRectangle(frame):
+	x,y,_ = np.argmax(frame[:,:,0],0)	
+	cv2.rectangle(frame, [x,y], [x,y]+10, [0,255,0])
+
+
+if __name__ == "__main__":
+	run()
