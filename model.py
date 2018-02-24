b3e0187b5f55decf24878b201fe5217c666dc0af
dan <dmsedra@gmail.com>
2018-02-23 18:21:00 -0800

    added fully conv and timer

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.contrib.layers import fully_connected, conv2d
+from tensorflow.contrib.layers import fully_connected, conv2d, max_pool2d
 
 def small_model(X,Y):
 	X = tf.reshape(X, [-1,32*32])
@@ -38,7 +38,27 @@ def small_conv_model(X,Y):
 
 	return [optimizer, loss, acc, probs]	
 
+def fully_conv_model(X,Y):
+	X = tf.reshape(X, [-1,32,32,1])
+	Y = tf.reshape(Y, [-1,2])
 
+	cv1 = conv2d(X, 2, [7,7], stride=3)
+	cv2 = conv2d(cv1, 2, [5,5], stride=3)
+	cv3 = conv2d(cv2, 2, [3,3], stride=1)
+	cv4 = conv2d(cv3, 2, [3,3], stride=1)
+	pool1 = max_pool2d(cv4, [4,4])
+
+	cv_reshape = tf.reshape(pool1, [-1,2])
+
+	probs = tf.nn.softmax(cv_reshape)
+
+	loss = tf.losses.log_loss(probs, Y)
+
+	optimizer = tf.train.AdamOptimizer().minimize(loss)
+
+	acc = tf.contrib.metrics.accuracy(tf.greater(Y, tf.constant(0.5)), tf.greater(probs, tf.constant(0.5)))
+
+	return [optimizer, loss, acc, probs]
 
 
 	
--- run.py
+++ run.py
@@ -2,6 +2,7 @@ from load_data import DataGenerator
 import numpy as np
 import model
 import tensorflow as tf
+import time
 
 def main(epochs=100):
 	params = {"faces_dir":"data/orl_faces", "cifar_file":"data/cifar_10/data_batch_1"}
@@ -10,20 +11,20 @@ def main(epochs=100):
 
 	X = tf.placeholder(tf.float32, [None, 32,32])
 	Y = tf.placeholder(tf.float32, [None,2])
-	optimizer,cost,acc,probs = model.small_conv_model(X,Y)
+	optimizer,cost,acc,probs = model.small_model(X,Y)
 
 	steps = int(np.ceil(dg.X.shape[0]/dg.batch_size)*epochs)
-	#init = tf.global_variables_initializer()
 	init = tf.global_variables_initializer()
 
 	saver = tf.train.Saver()
 	with tf.Session() as sess:
 		sess.run(init)
-
+		s = time.time()
 		for i in range(steps):
 			X_batch, Y_batch = next(gen) #np.concatenate((np.ones((1,250,250)),np.zeros((1,250,250)))), [[0,1],[1,0]]
 
 			#update params
+			
 			p,c,a = sess.run([optimizer,cost,acc], feed_dict={X:X_batch, Y:Y_batch})
 			print("Step %d Cost %f Accuracy %f"%(steps,c,a))
 			
@@ -32,6 +33,7 @@ def main(epochs=100):
 				X_eval,Y_eval = dg.eval()
 				eval_a = sess.run([acc], feed_dict={X:X_batch, Y:Y_batch})
 				print("Eval Accuracy %f"%eval_a[0])
+		print("Time %f"%(time.time()-s))
 
 		#saver.save(sess, "models/small/model.ckpt")
 
